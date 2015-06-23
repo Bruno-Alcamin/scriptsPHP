c8af12970c3a79f1f141654d95aa5aa46129e7ad
Bruno Alcamin Moreira <bruno_alcamin@hotmail.com>
2015-06-22 21:28:05 -0300

    Aulas de PHP

---
+++ aula11-ex02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+<title>Aula 11</title>
+<link href="_css/estilo.css" rel="stylesheet">
+</head>
+
+<body>
+<div>
+<h3>Curso de PHP – 11 – Estruturas de Repetição (Parte 1)</h3>
+
+<?php 
+	$c =10;
+	while($c>=1){
+		echo $c." ";
+		$c--;
+	}
+?>
+
+</div>
+</body>
+</html>
